7ec27a37f47c525a0b7b64bd23922ec0c064cb0b
Philipe Frazão Santos <156717680+younglipe@users.noreply.github.com>
2024-04-03 11:21:28 -0300

    correção de bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form');
 
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const weight = document.getElementById('weight').value;
@@ -17,17 +17,17 @@ form.addEventListener('submit', function(event) {
 
     if (imc < 18.5) {
         description = 'Cuidado você está abaixo do peso!'
-    } else if ( imc >= 18.5 && imc <= 25 ) {
-        description = 'Você está no peso ideal!'
-        }   else if ( imc > 25 && imc <= 30) {
-                description = 'Cuidado, você está com sobrepeso!'
-            }   else if ( imc > 30 && imc <= 35) {
-                    description = 'Cuidado, você está com obesidade moderada!'
-                } else if ( imc > 35 && imc <= 40) {
-                        description = 'Cuidado, você está com obesidade severa!'
-                    }   else {
-                            description = 'Cuidado, você está com obesidade morbida!'
-                    }  
+    } else if (imc >= 18.5 && imc <= 25) {
+        description = 'Você está no peso ideal!';
+    } else if (imc > 25 && imc <= 30) {
+        description = 'Cuidado, você está com sobrepeso!';
+    } else if (imc > 30 && imc <= 35) {
+        description = 'Cuidado, você está com obesidade moderada!';
+    } else if (imc > 35 && imc <= 40) {
+        description = 'Cuidado, você está com obesidade severa!';
+    } else {
+        description = 'Cuidado, você está com obesidade morbida!';
+    }
 
     value.textContent = imc.replace('.', ",");
     document.getElementById('description').textContent = description;
