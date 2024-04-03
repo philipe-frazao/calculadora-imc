652465d5695cf68902f376db2a9fc27358c0b479
Philipe Frazão Santos <156717680+younglipe@users.noreply.github.com>
2024-04-03 10:32:58 -0300

    atualização no js

--- script.js
+++ script.js
@@ -15,10 +15,21 @@ form.addEventListener('submit', function(event) {
     document.getElementById('infos').classList.remove('hidden');
 
 
-    if (imc <18.5) {
+    if (imc < 18.5) {
         description = 'Cuidado você está abaixo do peso!'
-    }
+    } else if ( imc >= 18.5 && imc <= 25 ) {
+        description = 'Você está no peso ideal!'
+        }   else if ( imc > 25 && imc <= 30) {
+                description = 'Cuidado, você está com sobrepeso!'
+            }   else if ( imc > 30 && imc <= 35) {
+                    description = 'Cuidado, você está com obesidade moderada!'
+                } else if ( imc > 35 && imc <= 40) {
+                        description = 'Cuidado, você está com obesidade severa!'
+                    }   else {
+                            description = 'Cuidado, você está com obesidade morbida!'
+                    }  
 
+    value.textContent = imc.replace('.', ",");
     document.getElementById('description').textContent = description;
 });
 
