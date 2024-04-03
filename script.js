ac9da81ed70c6ac3cca9375ffa5f7ebd028fe825
Philipe Frazão Santos <156717680+younglipe@users.noreply.github.com>
2024-04-03 11:32:39 -0300

    projeto versão 1.0

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ form.addEventListener('submit', function (event) {
     const value = document.getElementById('value');
     let description = '';
 
+    value.classList.add("attention");
+
     document.getElementById('infos').classList.remove('hidden');
 
 
@@ -19,6 +21,10 @@ form.addEventListener('submit', function (event) {
         description = 'Cuidado você está abaixo do peso!'
     } else if (imc >= 18.5 && imc <= 25) {
         description = 'Você está no peso ideal!';
+
+        value.classList.remove("attention");
+        value.classList.add("normal");
+
     } else if (imc > 25 && imc <= 30) {
         description = 'Cuidado, você está com sobrepeso!';
     } else if (imc > 30 && imc <= 35) {
